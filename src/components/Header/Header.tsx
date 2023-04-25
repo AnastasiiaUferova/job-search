e95c926c54ed9aa48e553cb9b5273d3208f9a306
‘Anastasiia <miss.iuferova@yandex.ru>
2023-04-25 17:35:50 +0200

    feat: add logo and navlonks to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import "./Header.css";
+import "../../styles/Header/Header.css";
 import { NavLink } from "react-router-dom";
 
 export default function Header(): JSX.Element {
   return (
     <div className="header">
-      <NavLink to="/">Поиск Вакансии</NavLink>
-      <NavLink to="/saved">Избранное</NavLink>
+      <div className="header__content">
+        <div className="header__logo">
+          <NavLink className="header__logo_title" to="/">
+            Jobored
+          </NavLink>
+        </div>
+        <div className="header__links">
+          <NavLink to="/">Поиск Вакансии</NavLink>
+          <NavLink to="/saved">Избранное</NavLink>
+        </div>
+      </div>
     </div>
   );
 }
