03fc56be62cbbe5d010deaa88f06f5b2040098d8
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-01 13:15:12 +0200

    fix: correct saved flexbox

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CardList from "../components/CardList/CardList";
+import "../styles/CardList/CardList.css";
+import "../styles/Card/Card.css";
 import "../styles/Saved/Saved.css";
 
 export default function Saved() {
   return (
     <div className="saved">
-      <CardList />
+      <div className="saved__wrapper">
+        <CardList />
+      </div>
     </div>
   );
 }
