af064665649d8384e40fb42f76f0a57965b422cb
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-02 16:42:22 +0200

    feat: add emty state component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import VacancyDetails from "./pages/VacancyDetails";
+import EmptyState from "./components/EmptyState/EmptyState";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/saved" element={<Saved />}></Route>
         <Route path="/details" element={<VacancyDetails />}></Route>
+        <Route path="/em" element={<EmptyState />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/EmptyState/NavButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mantine/core";
+import React from "react";
+
+export default function NavButton() {
+  return (
+    <>
+      <Button
+        styles={{
+          root: {
+            backgroundColor: "#DEECFF",
+            height: "42px",
+            width: "164px",
+            borderRadius: "8px",
+            color: "#3B7CD3",
+            border: "none",
+            fontFamily: "'Open Sans', sans-serif",
+            fontWeight: "normal",
+            fontSize: "14px",
+            lineHeight: "21,7px",
+            "&:hover, &:focus": {
+              backgroundColor: "#5E96FC",
+              color: "#FFF",
+            },
+          },
+        }}
+      >
+        Поиск Вакансий
+      </Button>
+    </>
+  );
+}
