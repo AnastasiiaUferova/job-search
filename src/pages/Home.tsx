dc541e5ccf294640a93d101443d1bbd076f5a1ad
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-17 12:28:50 +0200

    refactor: refactor App and components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "../styles/Home/Home.css";
 import Filter from "../components/Filter/Filter";
 import SearchComponent from "../components/SearchComponent/SearchComponent";
