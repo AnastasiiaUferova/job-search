47305fdadc08758204c1fb849aef37b79c84e6ec
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-18 18:02:53 +0200

    refactor: change to async

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,35 +3,32 @@ import { AUTH_URL, BASIC_URL, configAuth } from "../_constants/constants";
 import axios from "axios";
 
 export default function useAuth() {
-  const [error, setError] = useState(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [error, setError] = useState<any>(null);
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [ttl, setTtl] = useState<number>();
 
   const token = localStorage.getItem("token");
 
-  const fetchDataAuth = () => {
-    axios
-      .get(BASIC_URL + AUTH_URL, configAuth)
-      .then((res) => {
-        localStorage.setItem("token", res.data.access_token);
-        setLoggedIn(true);
-      })
-      .catch((err) => {
-        setError(err);
-        console.log(err);
-      });
+  const fetchDataAuth = async () => {
+    try {
+      const response = await axios.get(BASIC_URL + AUTH_URL, configAuth);
+      localStorage.setItem("token", response.data.access_token);
+      setLoggedIn(true);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
   };
 
-  const fetchDataTtl = () => {
-    axios
-      .get(BASIC_URL + AUTH_URL, configAuth)
-      .then((res) => {
-        setTtl(res.data.ttl);
-      })
-      .catch((err) => {
-        setError(err);
-        console.log(err);
-      });
+  const fetchDataTtl = async () => {
+    try {
+      const response = await axios.get(BASIC_URL + AUTH_URL, configAuth);
+      setTtl(response.data.ttl);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
   };
 
   const tokenCheck = () => {
