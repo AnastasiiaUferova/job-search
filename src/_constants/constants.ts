0962b626621d2c83c0846e21b662d8845d16647b
‘Anastasiia <miss.iuferova@yandex.ru>
2023-05-24 16:26:59 +0200

    fix: last fixes

--- src/_constants/constants.ts
+++ src/_constants/constants.ts
@@ -1,5 +1,3 @@
-import { X_API_APP_ID, X_SECRET_KEY } from "../env";
-
 export const BASE_URL =
   "https://startup-summer-proxy-production.up.railway.app";
 
@@ -18,12 +16,14 @@ export const NavLinksData = [
 ];
 
 export const headers = {
-  "x-secret-key": X_SECRET_KEY!,
-  "x-api-app-id": X_API_APP_ID!,
+  "x-secret-key": "GEU4nvd3rej*jeh.eqp",
+  "x-api-app-id":
+    "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948",
   Authorization: `Bearer ${localStorage.getItem("token")}`,
 };
 
 export const headersAuth = {
-  "x-secret-key": X_SECRET_KEY!,
-  "x-api-app-id": X_API_APP_ID!,
+  "x-secret-key": "GEU4nvd3rej*jeh.eqp",
+  "x-api-app-id":
+    "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948",
 };
--- src/env.ts
+++
@@ -1,2 +0,0 @@
-export const X_SECRET_KEY = import.meta.env.VITE_VERCEL_ENV_X_SECRET_KEY;
-export const X_API_APP_ID = import.meta.env.VITE_VERCEL_ENV_X_API_APP_ID;
